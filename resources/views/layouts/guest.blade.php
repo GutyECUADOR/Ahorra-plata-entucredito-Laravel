3751cf2ff09395ee00924b28ee9aa42140ce3ebf
José L. Gutiérrez <gutiecuador@gmail.com>
2023-09-21 23:28:20 -0500

    update welcome page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,8 @@
         <link href="assets/img/favicon.ico" rel="icon">
         <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
